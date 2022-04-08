1d46881ce444e55131afee700f5a7da9cce03583
your_name <your_email_id>
2022-04-08 01:17:34 -0400

    commit

---
+++ homework/home-work-day2/app.js
@@ -0,0 +1,19 @@
+
+
+const album= {
+
+    name: "Name of the album",
+    Singer: "name of the singer",
+    trackNumber: "the track number"
+    
+}
+
+
+function trackNumber(song){
+return song
+}
+
+for(let i=0; i<=12; i++){
+    console.log(i)
+}
+
